0c32f346c1bf1774f1bc7448afdd04d4a784b9f9
Kaic Nunes <kaicmurilo@live.com>
2023-01-15 21:29:49 -0400

    pagina de loguin e restricoes

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 
 class Jogo:
@@ -14,6 +14,7 @@ jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 listaJogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'kaicnunes'
 
 
 @app.route('/')
@@ -23,6 +24,9 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
+
     return render_template('novo.html', titulo='Novo Jogo')
 
 
@@ -35,6 +39,36 @@ def criar():
     listaJogos.append(jogo)
     return redirect('/')
 
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar',  methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        proxima_pagina = request.form['proxima']
+        if (proxima_pagina == 'None'):
+            return redirect('/')
+        return redirect('/{}'.format(proxima_pagina))
+    else:
+        flash('Erro em credenciais!')
+        return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Nao se encontra logado.')
+        return redirect('/')
+
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
 
     # trecho da app
 app.run(debug=True)
